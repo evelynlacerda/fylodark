00c4256282e07ac7806218bf0a9427d48c2a891f
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-09-15 06:05:09 -0300

    :sparkles: feat: add html, sass and js sheets

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.header, .intro-button, .one, .two, .info-img', {
+    duration: 1000
+});
+
+sr.reveal('.intro-img, .intro-title, .three, .four, .info-desc, .feedback-quote, .fb-one', { 
+    delay: 500,
+    duration: 1000
+});
+
+sr.reveal('.intro-text, .fb-two', { 
+    delay: 1000,
+    duration: 1000
+});
+
+sr.reveal('.fb-three', { 
+    delay: 1500,
+    duration: 1000
+});
