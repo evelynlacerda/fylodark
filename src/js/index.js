eaca54942f82e3d86f87efeb372ffd07590272db
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-09-16 22:08:47 -0300

    :lipstick: feat: fished footer and add background image

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.header, .intro-button, .one, .two, .info-img', {
+sr.reveal('.header, .intro-button, .one, .two, .info-img, .feedback-quote, .footer-img', {
     duration: 1000
 });
 
-sr.reveal('.intro-img, .intro-title, .three, .four, .info-desc, .feedback-quote, .fb-one', { 
+sr.reveal('.intro-img, .intro-title, .three, .four, .info-desc, .fb-one, .footer-content', { 
     delay: 500,
     duration: 1000
 });
 
-sr.reveal('.intro-text, .fb-two', { 
+sr.reveal('.intro-text, .fb-two, .cta-box', { 
     delay: 1000,
     duration: 1000
 });
