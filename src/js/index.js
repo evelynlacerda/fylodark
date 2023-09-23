02acc0552a75e6e380cbbde13ac7db563249a8c9
Evelyn Lacerda <evelyndslacerda@gmail.com>
2023-09-23 05:23:11 -0300

    :iphone: feat: added responsive for mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,15 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.header, .intro-button, .one, .two, .info-img, .feedback-quote, .footer-img', {
-    duration: 1000
-});
+sr.reveal('.header, .intro-button, .one, .two, .info-img, .feedback-quote, .footer-img');
 
 sr.reveal('.intro-img, .intro-title, .three, .four, .info-desc, .fb-one, .footer-content', { 
-    delay: 500,
-    duration: 1000
+    delay: 500
 });
 
 sr.reveal('.intro-text, .fb-two, .cta-box', { 
-    delay: 1000,
-    duration: 1000
+    delay: 1000
 });
 
 sr.reveal('.fb-three', { 
-    delay: 1500,
-    duration: 1000
+    delay: 1500
 });
